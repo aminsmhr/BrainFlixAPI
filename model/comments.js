4fc79381b3babcfec9c733d361b607438bed9650
Amin Saeedmehr <amin.smhr@gmail.com>
2023-12-11 00:03:44 -0800

    added comment creation and comment template

--- model/comments.js
+++ model/comments.js
@@ -4,9 +4,18 @@ const { DATA_FILE } = process.env;
 const VIDEOS_PATH = DATA_FILE;
 
 function getVideoComments(id, api_key) {
+  console.log("listing comments for :", id, api_key);
   const file = fs.readFileSync(VIDEOS_PATH);
   const videos = JSON.parse(file);
-  return videos[api_key].find((video) => video.id === id).comments;
+  let video = videos[api_key].find((video) => video.id == id);
+  console.log("amin", id, "====> ", video);
+
+  if (!Object.keys(video).includes("comments")) {
+    console.log("comments not found");
+    video.comments = [];
+    console.log(videos[api_key]);
+  }
+  return video.comments;
 }
 
 function getAllVideos() {
@@ -17,8 +26,11 @@ function getAllVideos() {
 
 function createComment(id, comment, api_key) {
   const videos = getAllVideos();
-  videoFound = videos[api_key].find((video) => video.id === id);
+  videoFound = videos[api_key].find((video) => video.id == id);
+  console.log(videoFound);
   if (videoFound) {
+    videoFound.comments ??= [];
+    console.log("videoFound", videoFound);
     videoFound.comments.push(comment);
     fs.writeFileSync(VIDEOS_PATH, JSON.stringify(videos));
   } else {
@@ -28,11 +40,12 @@ function createComment(id, comment, api_key) {
 
 function deleteComment(id, commentId, api_key) {
   const videos = getAllVideos();
-  const videoFound = videos[api_key].find((video) => video.id === id);
+  const videoFound = videos[api_key].find((video) => video.id == id);
   if (videoFound) {
     const commentIndex = videoFound.comments.findIndex(
-      (comment) => comment.id === commentId
+      (comment) => comment.id == commentId
     );
+    console.log(commentId, commentIndex);
     if (commentIndex !== -1) {
       videoFound.comments.splice(commentIndex, 1);
       fs.writeFileSync(VIDEOS_PATH, JSON.stringify(videos));
