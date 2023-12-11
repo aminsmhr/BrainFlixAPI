47ae0d255d45557b92da732f6df3281076b8f796
Amin Saeedmehr <amin.smhr@gmail.com>
2023-12-10 20:38:10 -0800

    added multi tenancy to videos model

--- model/videos.js
+++ model/videos.js
@@ -1,32 +1,40 @@
 const fs = require("node:fs");
-
-const VIDEOS_PATH = "./data/videos.json";
+require("dotenv").config();
+const { DATA_FILE } = process.env;
+const VIDEOS_PATH = DATA_FILE;
 
 // the 'model' is where we interact with the resource (ie the 'videos' themselves),
 // to send to the controller (which then sends the info off to the client/view)
 
-function getVideos() {
+function getVideos(api_key) {
+  const file = fs.readFileSync(VIDEOS_PATH);
+  const videos = JSON.parse(file);
+  return videos[api_key];
+}
+
+function getAllVideos() {
   const file = fs.readFileSync(VIDEOS_PATH);
   const videos = JSON.parse(file);
   return videos;
 }
 
-function getOneVideo(id) {
+function getOneVideo(id, api_key) {
   // notice we are using 'getVideos' here
-  const Videos = getVideos();
-  const video = videos.find((video) => video.id === id);
+  const videos = getVideos(api_key);
+  const video = videos[api_key].find((video) => video.id === id);
   return video;
 }
 
-function createVideo(video) {
+function createVideo(video, api_key) {
   // notice we are using 'getVideos' here too
-  const videos = getVideos();
-  videos.push(video);
+  const videos = getAllVideos();
+  videos[api_key].push(video);
   fs.writeFileSync(VIDEOS_PATH, JSON.stringify(videos));
 }
 
 module.exports = {
   getVideos,
+  getAllVideos,
   createVideo,
   getOneVideo,
 };
