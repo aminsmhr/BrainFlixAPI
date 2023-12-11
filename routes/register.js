f90b19ac1efaa5c1ba6857219cf34e19eb32f024
Amin Saeedmehr <amin.smhr@gmail.com>
2023-12-11 00:04:51 -0800

    removed jon's comment from his boiler plate code

--- routes/register.js
+++ routes/register.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const { get } = require("../controllers/registerController");
-
-// We define paths in this file.
-// The 'validator' middleware checks if the request has the required fields for a video.
 router.route("/").get(get);
 
 module.exports = router;
