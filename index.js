f255fe2e0015111774569e022f808eef94f5bf3f
Amin Saeedmehr <amin.smhr@gmail.com>
2023-12-10 20:36:43 -0800

    refactored routes

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const dogFacts = require("./routes/dog-facts");
-const noteRoutes = require("./routes/notes");
+const videoRoutes = require("./routes/videos");
+const registerRoutes = require("./routes/register");
 
 const app = express();
-
-// we are destructuring the PORT env (see .env file)
 const { PORT } = process.env;
 
 // app.use(cors({origin: process.env.CORS_ORIGIN}))
 // above will allow cors only from provided origin
 app.use(cors());
-
 app.use(express.json());
 
-app.use("/videos", noteRoutes);
+app.use("/videos", videoRoutes);
+app.use("/register", registerRoutes);
 
 // note how 'succint' this file is. it tells us the 'basics':
 // enables cores, mounts some route files, and listens for connections on a port.  That's it!
